d0ba63c054a549f4d27eb7a6c97547ad68812410
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-18 02:58:57 +0530

    called query in the rootContainer and set + tested the response

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { queryData } from './query';
 
-class RootContainer extends React.Component {
-	render() {
-		return (
-			<div className="rootContainer">
-				<h1>Your Data Viz Here</h1>
-			</div>
-		);
-	}
-}
+const RootContainer = ({ serviceUrl, entity }) => {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		let { value } = entity;
+		queryData({
+			serviceUrl: serviceUrl,
+			geneId: value
+		}).then(data => setData(data));
+	}, []);
+	return (
+		<div className="rootContainer">
+			{data.length ? <h1>Your Data Viz Here</h1> : <h1>Loading</h1>}
+		</div>
+	);
+};
 
 export default RootContainer;
