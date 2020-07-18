2750f26eb304693c7a36911337d33c28ef25bcf0
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-18 05:53:04 +0530

    converted the data in the desired format required by the scatter plot

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -3,6 +3,7 @@ import { queryData } from './query';
 
 const RootContainer = ({ serviceUrl, entity }) => {
 	const [data, setData] = useState([]);
+	const [graphData, setGraphData] = useState([]);
 
 	useEffect(() => {
 		let { value } = entity;
@@ -11,9 +12,35 @@ const RootContainer = ({ serviceUrl, entity }) => {
 			geneId: value
 		}).then(data => setData(data));
 	}, []);
+
+	useEffect(() => {
+		const obj = {};
+		data.forEach(d => {
+			d.results.forEach(r => {
+				const { primaryIdentifier } = r.phenotype;
+				const { chromosome, chromosomeLocation } = r.marker;
+				if (!obj[primaryIdentifier])
+					obj[primaryIdentifier] = {
+						id: primaryIdentifier,
+						data: []
+					};
+				const allDigits = chromosome.secondaryIdentifier.match(/\d+/g);
+				const xAxisVal = allDigits[allDigits.length - 1] * 1;
+				obj[primaryIdentifier].data.push({
+					x: -1 * Math.log10(r.pValue),
+					y: xAxisVal + chromosomeLocation.start / chromosome.length
+				});
+			});
+		});
+		setGraphData([...Object.values(obj)]);
+	}, [data]);
 	return (
 		<div className="rootContainer">
-			{data.length ? <h1>Your Data Viz Here</h1> : <h1>Loading</h1>}
+			{data.length && graphData.length ? (
+				<h1>Your Data Viz Here</h1>
+			) : (
+				<h1>Loading</h1>
+			)}
 		</div>
 	);
 };
