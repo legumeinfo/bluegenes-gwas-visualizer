99a11ad934bb3b45d5a6f34c95433d0107a4b6a5
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-22 16:30:16 +0530

    conditionally formatting the query data in the graph form.

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -25,33 +25,56 @@ const RootContainer = ({ serviceUrl, entity }) => {
 		const obj = {};
 		data.forEach(d => {
 			d.results.forEach(r => {
-				if (
-					!r.marker ||
-					!r.pValue ||
-					!r.marker.chromosome ||
-					!r.marker.chromosomeLocation
-				)
-					return;
-				const { primaryIdentifier } = r.phenotype;
-				const { chromosome, chromosomeLocation } = r.marker;
-				if (!obj[primaryIdentifier])
-					obj[primaryIdentifier] = {
-						id: primaryIdentifier,
-						data: []
-					};
-				const allDigits = chromosome.secondaryIdentifier.match(/\d+/g) || [];
-				const xAxisVal = allDigits.length
-					? allDigits[allDigits.length - 1] * 1 - 1
-					: 0;
-				obj[primaryIdentifier].data.push({
-					x: xAxisVal + chromosomeLocation.start / chromosome.length,
-					y: -1 * Math.log10(r.pValue),
-					tooltip: r.marker && r.marker.primaryIdentifier
-				});
+				if (serviceUrl == 'https://www.humanmine.org/humanmine') {
+					if (!r.associatedGenes || !r.pValue) return;
+					r.associatedGenes.forEach(c => {
+						if (!c.chromosome || !c.chromosomeLocation) return;
+						const { primaryIdentifier, length } = c.chromosome;
+						const { start } = c.chromosomeLocation;
+						if (!obj[r.phenotype])
+							obj[r.phenotype] = {
+								id: r.phenotype,
+								data: []
+							};
+						const allDigits = primaryIdentifier.match(/\d+/g) || [];
+						const xAxisVal = allDigits.length
+							? allDigits[allDigits.length - 1] * 1 - 1
+							: 0;
+						obj[r.phenotype].data.push({
+							x: xAxisVal + start / length,
+							y: -1 * Math.log10(r.pValue)
+						});
+					});
+				} else {
+					if (
+						!r.marker ||
+						!r.pValue ||
+						!r.marker.chromosome ||
+						!r.marker.chromosomeLocation
+					)
+						return;
+					const { primaryIdentifier } = r.phenotype;
+					const { chromosome, chromosomeLocation } = r.marker;
+					if (!obj[primaryIdentifier])
+						obj[primaryIdentifier] = {
+							id: primaryIdentifier,
+							data: []
+						};
+					const allDigits = chromosome.secondaryIdentifier.match(/\d+/g) || [];
+					const xAxisVal = allDigits.length
+						? allDigits[allDigits.length - 1] * 1 - 1
+						: 0;
+					obj[primaryIdentifier].data.push({
+						x: xAxisVal + chromosomeLocation.start / chromosome.length,
+						y: -1 * Math.log10(r.pValue),
+						tooltip: r.marker && r.marker.primaryIdentifier
+					});
+				}
 			});
 		});
 		setGraphData([...Object.values(obj)]);
 	}, [data]);
+
 	return (
 		<div className="rootContainer">
 			<span className="chart-title">GWAS Visualizer</span>
