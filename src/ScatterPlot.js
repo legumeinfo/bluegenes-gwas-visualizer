656085b517e93b8f0a2bbb5a33b4d38201e5aacd
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-30 09:29:02 +0530

    removed nivo legend and added custom legend at the same place

--- src/ScatterPlot.js
+++ src/ScatterPlot.js
@@ -2,106 +2,106 @@ import React from 'react';
 import { ScatterPlot } from '@nivo/scatterplot';
 
 const Scatterplot = ({ graphData, minAxis }) => (
-	<ScatterPlot
-		data={graphData}
-		height={window.innerHeight - 150}
-		width={window.innerWidth - 50}
-		margin={{ top: 100, right: 90, bottom: 60, left: 90 }}
-		xScale={{
-			type: 'linear',
-			min: Math.round(minAxis.minX) - 5,
-			max: Math.round(minAxis.maxX) + 5
-		}}
-		yScale={{
-			type: 'linear',
-			min: Math.round(minAxis.minY) - 5,
-			max: Math.round(minAxis.maxY) + 5
-		}}
-		useMesh={false}
-		axisTop={null}
-		axisRight={null}
-		nodeSize={15}
-		colors={{ scheme: 'set1' }}
-		blendMode="multiply"
-		tooltip={({ node }) => (
-			<div
-				style={{
-					background: 'white',
-					color: 'inherit',
-					fontSize: 'inherit',
-					borderRadius: 2,
-					boxShadow: 'rgba(0, 0, 0, 0.25) 0px 1px 2px',
-					padding: '5px 9px'
-				}}
-			>
+	<>
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'space-evenly',
+				flexFlow: 'wrap',
+				marginTop: 20,
+				fontFamily: 'sans-serif',
+				fontSize: 13
+			}}
+		>
+			{graphData.map(data => (
+				<div key={data.id}>
+					<span>{data.id}</span>
+				</div>
+			))}
+		</div>
+		<ScatterPlot
+			data={graphData}
+			height={window.innerHeight - 150}
+			width={window.innerWidth - 50}
+			margin={{ top: 30, right: 90, bottom: 60, left: 90 }}
+			xScale={{
+				type: 'linear',
+				min: Math.round(minAxis.minX) - 5,
+				max: Math.round(minAxis.maxX) + 5
+			}}
+			yScale={{
+				type: 'linear',
+				min: Math.round(minAxis.minY) - 5,
+				max: Math.round(minAxis.maxY) + 5
+			}}
+			useMesh={false}
+			axisTop={null}
+			axisRight={null}
+			nodeSize={15}
+			colors={{ scheme: 'set1' }}
+			blendMode="multiply"
+			tooltip={({ node }) => (
 				<div
-					style={{ whiteSpace: 'pre', display: 'flex', alignItems: 'center' }}
+					style={{
+						background: 'white',
+						color: 'inherit',
+						fontSize: 'inherit',
+						borderRadius: 2,
+						boxShadow: 'rgba(0, 0, 0, 0.25) 0px 1px 2px',
+						padding: '5px 9px'
+					}}
 				>
-					<span
+					<div
 						style={{
-							display: 'block',
-							width: 12,
-							height: 12,
-							background: node.style.color,
-							marginRight: 7
+							whiteSpace: 'pre',
+							display: 'flex',
+							alignItems: 'center'
 						}}
-					></span>
-					<span>
-						<strong>{node.data.serieId}: </strong>
-						{node.data.tooltip}
-					</span>
+					>
+						<span
+							style={{
+								display: 'block',
+								width: 12,
+								height: 12,
+								background: node.style.color,
+								marginRight: 7
+							}}
+						></span>
+						<span>
+							<strong>{node.data.serieId}: </strong>
+							{node.data.tooltip}
+						</span>
+					</div>
+					<div>
+						<strong>Marker Position: </strong>
+						{node.data.x}
+					</div>
+					<div>
+						<strong>log10(p-value): </strong>
+						{node.data.y}
+					</div>
 				</div>
-				<div>
-					<strong>Marker Position: </strong>
-					{node.data.x}
-				</div>
-				<div>
-					<strong>log10(p-value): </strong>
-					{node.data.y}
-				</div>
-			</div>
-		)}
-		axisBottom={{
-			orient: 'bottom',
-			tickSize: 5,
-			tickPadding: 5,
-			tickRotation: 0,
-			legend: 'Chromosome',
-			legendPosition: 'middle',
-			legendOffset: 46
-		}}
-		axisLeft={{
-			orient: 'left',
-			tickSize: 5,
-			tickPadding: 5,
-			tickRotation: 0,
-			legend: '-log10(p)',
-			legendPosition: 'middle',
-			legendOffset: -60
-		}}
-		legends={[
-			{
-				anchor: 'top-left',
-				direction: 'row',
-				translateX: 0,
-				translateY: -60,
-				itemWidth: 250,
-				itemHeight: 12,
-				itemsSpacing: 5,
-				itemDirection: 'left-to-right',
-				symbolSize: 12,
-				symbolShape: 'circle',
-				effects: [
-					{
-						on: 'hover',
-						style: {
-							itemOpacity: 1
-						}
-					}
-				]
-			}
-		]}
-	/>
+			)}
+			axisBottom={{
+				orient: 'bottom',
+				tickSize: 5,
+				tickPadding: 5,
+				tickRotation: 0,
+				legend: 'Chromosome',
+				legendPosition: 'middle',
+				legendOffset: 46
+			}}
+			axisLeft={{
+				orient: 'left',
+				tickSize: 5,
+				tickPadding: 5,
+				tickRotation: 0,
+				legend: '-log10(p)',
+				legendPosition: 'middle',
+				legendOffset: -60
+			}}
+		/>
+	</>
 );
 
 export default Scatterplot;
